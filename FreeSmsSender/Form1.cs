197bb379a01584723c819162b225a15d768a0389
Amine Smahi <mohammed.amine.smahi@gmail.com>
2016-12-31 21:24:40 +0100

    added files

---
+++ FreeSmsSender/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+
+namespace FreeSmsSender
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    { 
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x39000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.Cyan500, Primary.Cyan700, Primary.Cyan500, Accent.Cyan200, TextShade.WHITE);
+            
+      
+        }
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "" || txtPass.Text == "")
+            {
+                MessageBox.Show("Pleas enter a valide username or password");
+            }
+            else
+            {
+                new Form2().Show();
+                this.Visible = false;
+            }
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            if(txtUser.Text == "" || txtPass.Text==""){
+                MessageBox.Show("Pleas enter a valide username or password");
+            }
+            else{
+                new Form2().Show();
+                this.Visible = false;
+            }
+           
+        }
+
+        private void materialRaisedButton3_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+        }
+    }
+}
---
+++ FreeSmsSender/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using System.Net;
+using System.Net.Mail;
+
+namespace FreeSmsSender
+{
+    public partial class Form2 : MaterialSkin.Controls.MaterialForm
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x39000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.Cyan500, Primary.Cyan700, Primary.Cyan500, Accent.Cyan200, TextShade.WHITE);
+
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Visible = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try {
+                Form1 f1 =  new Form1();
+               
+                SmtpClient smtp = new SmtpClient();
+                MailMessage message = new MailMessage();
+                smtp.Credentials = new NetworkCredential(f1.txtUser.Text, f1.txtPass.Text);
+                smtp.Host = "ipipi.com";
+                message.From = new MailAddress(string.Format("{0}@sms.ipipi.com"),f1.txtUser.Text);
+                message.To.Add(string.Format("{0}@sms.ipipi",txtPhone.Text));
+                message.Subject = "Free sms sender by amine smahi";
+                message.Body = txtMess.Text;
+                smtp.Send(message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mesage", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
