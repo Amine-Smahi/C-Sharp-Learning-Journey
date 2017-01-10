e6a0cfd3f7a2d4fec745558d25caf39ab847bdcc
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-01-10 23:18:15 +0100

    Update Form2.cs

--- FreeSmsSender/Form2.cs
+++ FreeSmsSender/Form2.cs
@@ -44,17 +44,7 @@ namespace FreeSmsSender
         private void btnSend_Click(object sender, EventArgs e)
         {
             try {
-                Form1 f1 =  new Form1();
-               
-                SmtpClient smtp = new SmtpClient();
-                MailMessage message = new MailMessage();
-                smtp.Credentials = new NetworkCredential(f1.txtUser.Text, f1.txtPass.Text);
-                smtp.Host = "ipipi.com";
-                message.From = new MailAddress(string.Format("{0}@sms.ipipi.com"),f1.txtUser.Text);
-                message.To.Add(string.Format("{0}@sms.ipipi",txtPhone.Text));
-                message.Subject = "Free sms sender by amine smahi";
-                message.Body = txtMess.Text;
-                smtp.Send(message);
+              
             }
             catch(Exception ex)
             {
