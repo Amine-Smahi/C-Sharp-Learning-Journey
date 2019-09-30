ff15008f2699b6e34413a99a2558016a661cecc8
Amine Smahi <mohammed.amine.smahi@gmail.com>
2019-09-30 21:04:31 +0100

    Add my first bot project

---
+++ MyFirstBot/Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+           
+            switch (activity.Text)
+            {
+                case "Salem": case "salem":
+                    await context.PostAsync($"wa 3alaykom salem");
+                    break;
+                case "cv ?": case "Cv":  case "cv":
+                    await context.PostAsync($"el Hamdouli allah w nta ?");
+                    break;
+                case "kech jdid":
+                    await context.PostAsync($"wlh walooo");
+                    break;
+                case "hmd":
+                case "hmdlh":
+                case "el hamdouli allah":
+                    await context.PostAsync($"{activity.Text}");
+                    break;
+                   
+                case "wech tgol l team open mind ?":
+                    await context.PostAsync($"Saha ftorkom !");
+                    break;
+                case "sahit":
+                    await context.PostAsync($"hafdeh ;-)");
+                    break;
+                default:
+                    {
+                        await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+                        break;
+                    }
+            }
+           
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ MyFirstBot/README.md
@@ -0,0 +1,10 @@
+# MyFirstBot
+My first .NET bot using microsoft bot builder , the bot respond to predifined Questions.
+
+### How to create your own bot
+
+Quick start from microsoft  https://docs.microsoft.com/en-us/bot-framework/dotnet/bot-builder-dotnet-quickstart
+
+### Some Screenshots
+
+![Microsoft .net bot builder simple](https://cloud.githubusercontent.com/assets/24621701/26766405/048d5d66-4946-11e7-834e-576e59ca1340.jpg)
