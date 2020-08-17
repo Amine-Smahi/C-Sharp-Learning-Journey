fc02024c634a76eefd0fdd5436b4e855cd89c679
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-08-17 10:26:38 +0100

    Add iphoneX web previewer project

---
+++ IphoneXPreviewer/IphoneXPreviewer/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Logique d'interaction pour HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            var desktopWorkingArea = SystemParameters.WorkArea;
+            Left = desktopWorkingArea.Right - this.Width - 20;
+            Top = desktopWorkingArea.Bottom - this.Height - 100;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow(ClientUrl.Text);
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ IphoneXPreviewer/IphoneXPreviewer/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CefSharp;
+using System.Windows;
+
+namespace WpfApp5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string url)
+        {
+            InitializeComponent();
+            var desktopWorkingArea = SystemParameters.WorkArea;
+            Left = desktopWorkingArea.Right - this.Width - 20;
+            Top = desktopWorkingArea.Bottom - this.Height + 200;
+            Browser.Address = url;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (switcher.Content == "Add border")
+            {
+                switcher.Content = "Remove border";
+                borderimage.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                switcher.Content = "Add border";
+                borderimage.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Browser.Reload(false);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Browser.Reload(true);
+        }
+    }
+}
